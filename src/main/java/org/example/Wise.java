0fe2858da2ad12281e265c2cf7f872c67c3c2519
YunJiW <dtroy371@gmail.com>
2023-03-02 10:26:30 +0900

    level7

---
+++ src/main/java/org/example/Wise.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Wise {
+    private String name;
+    private String talked;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTalked() {
+        return talked;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTalked(String talked) {
+        this.talked = talked;
+    }
+}
