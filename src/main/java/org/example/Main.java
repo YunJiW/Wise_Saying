dea4b118eeef9dc5769a9be683fecc071f88668a
YunJiW <dtroy371@gmail.com>
2023-03-02 09:15:38 +0900

    level1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("== 명언 앱 ==");
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.print("명령) ");
+            String s = sc.nextLine();
+
+            if(s.equals("종료"))
+                break;
+        }
     }
 }
